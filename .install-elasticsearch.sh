f2bd42a1ac5e3acadbba0c3dd55e3c09dce3addc
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2022-04-25 15:41:05 +0200

    es heap size

--- .install-elasticsearch.sh
+++ .install-elasticsearch.sh
@@ -1,17 +1,4 @@
 #!/bin/bash -v
-function install_from_zip {
-  INSTALL_PATH=$HOME/es/$2
-  echo $INSTALL_PATH
-  if [ ! -d "$INSTALL_PATH" ]; then
-    wget -nv $1 -O out
-    unzip out
-    mv $2 $INSTALL_PATH
-  fi
-  $INSTALL_PATH/bin/elasticsearch -Vv
-  remove_if_erroneus $INSTALL_PATH
-  rm out
-}
-
 function install_from_targz {
   INSTALL_PATH=$HOME/es/$2
   echo $INSTALL_PATH
@@ -35,7 +22,6 @@ if [ ! -f "$HOME/es" ]; then
   mkdir $HOME/es
 fi
 
-install_from_zip https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.18.zip elasticsearch-6.8.18
 install_from_targz https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.14.2-linux-x86_64.tar.gz elasticsearch-7.14
 install_from_targz https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.2-linux-x86_64.tar.gz elasticsearch-7.15
 install_from_targz https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.16.2-linux-x86_64.tar.gz elasticsearch-7.16
